90e72be85e34d889b89254c4d5f127c7d2a0126b
Dimitri <dimitri1997NL@hotmail.nl>
2021-01-13 14:15:52 +0100

    deploy

--- killer-app/front-end/deploy.sh
+++ killer-app/front-end/deploy.sh
@@ -12,7 +12,6 @@ cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git init
 git add -A
 git commit -m 'deploy'
 
@@ -22,4 +21,3 @@ git commit -m 'deploy'
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:DimitriSmits/killerapp.git master:gh-pages
 
-cd -
